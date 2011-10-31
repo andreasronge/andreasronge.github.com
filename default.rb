c801ab2b1713f1aa04f8083d85aff94ec8b02585
Andreas Ronge <andreas.ronge@gmail.com>
2011-10-31 14:05:46 +0100

    Use 1.2.5

--- default.rb
+++ default.rb
@@ -1,4 +1,4 @@
-NEO4J_VERSION = "1.2.3"
+NEO4J_VERSION = "1.2.5"
 
 gsub_file 'Gemfile', /gem 'sql.*/, "gem 'neo4j', '#{NEO4J_VERSION}'"
 gsub_file 'Gemfile', /gem 'activerecord.*/, "gem 'neo4j', '#{NEO4J_VERSION}'"
